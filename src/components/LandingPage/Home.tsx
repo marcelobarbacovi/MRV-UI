0680c1cc2fe1f67ff2f8dc3f0b2c2e5decbefa23
marcelo <barbacovi182@gmail.com>
2023-07-02 13:35:14 -0300

    alteracoes site

--- src/components/LandingPage/Home.tsx
+++ src/components/LandingPage/Home.tsx
@@ -4,21 +4,17 @@ import { Button, Carousel } from 'antd';
 const initItems = [
   {
     key: '1',
-    title: 'Web and mobile payment built for developers',
+    title: 'Precisando realizar uma consultoria tributária?',
     content:
-      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
+      'Venha ser parceiro de quem entende do assunto, temos uma equipe especializada para te ajudar.',
+    style: 'font-size: 30px;', // Defina o tamanho da fonte usando CSS
   },
   {
     key: '2',
-    title: 'Work better together. Schedule meetings',
+    title: 'Tem algumas dúvidas sobre retenção de serviços?',
     content:
-      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
-  },
-  {
-    key: '3',
-    title: 'The best app to increase your productivity',
-    content:
-      'An vim odio ocurreret consetetur, justo constituto ex mea. Quidam facilisis vituperata pri ne. Id nostrud gubergren urbanitas sed, quo summo animal qualisque ut, cu nostro dissentias consectetuer mel. Ut admodum conceptam mei, cu eam tation fabulas abhorreant. His ex mandamus.',
+      'Consulte nosso material das principais dúvidas encontradas a respeito de retenção de serviços. Venha conferir.',
+    style: 'font-size: 25px;', // Defina o tamanho da fonte usando CSS
   },
 ];
 
@@ -40,10 +36,7 @@ export const Home = () => {
                 <p>{item.content}</p>
                 <div className="btnHolder">
                   <Button type="primary" size="large">
-                    Learn More
-                  </Button>
-                  <Button size="large">
-                    <i className="fas fa-desktop"></i> Watch a Demo
+                    Faça uma consulta tributária
                   </Button>
                 </div>
               </div>
